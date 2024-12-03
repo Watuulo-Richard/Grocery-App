dc432c20e2155f252ab2bc0429127cd1d3e0757d
Watuulo-Richard <watuulorichard@gmail.com>
2024-12-03 17:55:46 +0300

    Third-Commit

--- app.js
+++ app.js
@@ -63,48 +63,114 @@ const cardItems = cardsContainerArray.forEach((card) => {
         cardsContainer.insertAdjacentHTML('afterbegin', cardItem)
 })
 
-const cardContainerTwoArray = [{
-    {
-        id: 1,
-        productName: 'Vegetable',
-        productLocation: 'Local market',
-        cardImage: './Images/brocolli-icon.jpg'
-    },
-    {
-        id: 2,
-        productName: 'Snacks & Breads',
-        productLocation: 'In store delivery',
-        cardImage: './Images/9851130.jpg'
-    },
-    {
-        id: 3,
-        productName: 'Fruits',
-        productLocation: 'Conical Free',
-        cardImage: './Images/18965845.jpg'
-    },
-    {
-        id: 4,
-        productName: 'Chicken Legs',
-        productLocation: 'Frozen Meal',
-        cardImage: './Images/Tasty fried chicken -1.jpg'
-    },
-    {
-        id: 5,
-        cardImage: "./Images/beetroot.jpg",
-        cardName: 'Milk & Dairy',
-        cardLocation: 'Process Food',
-    },
-}]
+// const cardContainerTwoArray = [
+//     {
+//         id: 1,
+//         cardImage: "./Images/beetroot.jpg",
+//         cardName: 'Milk & Dairy',
+//         cardLocation: 'Process Food',
+//         card: '500 gm.',
+//         cardPrice: '17. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 2,
+//         cardImage: "./Images/fresh-meat-slices-raw-meat-with-leaves-top-view-transparent-background_84443-1899.jpg",
+//         cardName: 'Italian Avacado',
+//         cardLocation: 'Local shop',
+//         card: '500 gm.',
+//         cardPrice: '12. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 3,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Szam amm',
+//         cardLocation: 'Process food',
+//         card: '500 gm.',
+//         cardPrice: '14. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 4,
+//         cardImage: "./Images/beetroot.jpg",
+//         cardName: 'Beef Mixed',
+//         cardLocation: 'Cut Bone',
+//         card: '500 gm.',
+//         cardPrice: '16. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 5,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Beef Mixed',
+//         cardLocation: 'Cut Bone',
+//         card: '500 gm.',
+//         cardPrice: '18. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 6,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Plant Hunter',
+//         cardLocation: 'Frozen pack',
+//         card: '500 gm.',
+//         cardPrice: '17. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 7,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Deshi Gajor',
+//         cardLocation: 'Local Carrot',
+//         card: '500 gm.',
+//         cardPrice: '12. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 8,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Deshi Shosha',
+//         cardLocation: 'Local Cucumb',
+//         card: '500 gm.',
+//         cardPrice: '14. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 9,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Lays chips',
+//         cardLocation: 'Bacon',
+//         card: '500 gm.',
+//         cardPrice: '16. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+//     {
+//         id: 10,
+//         cardImage: "./Images/3d-red-soda-can-isolated-beer-cola-tin-bottle_107791-26875.jpg",
+//         cardName: 'Beef Mixed',
+//         cardLocation: 'Cut Bone',
+//         card: '500 gm.',
+//         cardPrice: '18. <sup>29$</sup>',
+//         cardIcon: '<i class="fa-solid fa-plus"></i>'
+//     },
+// ]
 
-<div class="card-item">
-                    <div class="card-img">
-                        <img src="./Images/beetroot.jpg" alt="">
-                    </div>
-                    <h5>Beetroot</h5>
-                    <p>(Local shop)</p>
-                    <h6>500 gm.</h6>
-                    <p>17. <sup>29$</sup></p>
-                    <div class="card-icon-container">
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                </div>
+
+// const cardItemsTwo = cardContainerTwoArray.forEach((cardTwo) => {
+//                     const cardItemTwo = `
+//                     <div class="card-item">
+//                         <div class="card-img">
+//                             <img src="${cardTwo.cardImage}" alt="">
+//                         </div>
+//                         <h5>${cardTwo.cardName}</h5>
+//                         <p>(${cardTwo.cardLocation})</p>
+//                         <h6>${cardTwo.cardDetail}</h6>
+//                         <p>${cardTwo.cardPrice}</p>
+//                         <div class="card-icon-container">
+//                         ${cardTwo.cardIcon}
+//                         </div>
+//                     </div>
+//                     `
+//                     cardsContainer.insertAdjacentHTML('afterbegin', cardItemTwo)
+//  })
